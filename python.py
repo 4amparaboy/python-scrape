5ff78d31fa2e6b78e513fff795082066b620bb55
Amista <mystoganspace@gmail.com>
2020-06-12 05:16:11 +0700

    remove comment

--- python.py
+++ python.py
@@ -24,63 +24,6 @@ for row in oke:
         ramalan = row("td",{"style":"text-align: left; vertical-align: middle;"})[i].text
         ramalan_2 = re.sub(r'\u00b0|\s+$|^\W+', r'', ramalan)
         res['Ramalan'] = re.split(r'\n', ramalan_2)
-        #xxx = res["Jam"].find("td",{"style":"text-align: center; vertical-align: middle;"})[i].text
         x.append(res)
 
 print(json.dumps(x, indent=4))
-
-
-# import bs4
-# from urllib.request import urlopen as uReq
-# from bs4 import BeautifulSoup as soup
-# my_url= 'http://www.met.gov.my/forecast/weather/town'
-# uClient= uReq(my_url)
-# page_html = uClient.read() 
-# uClient.close()
-# page_soup = soup(page_html,"html.parser")
-# containers = page_soup.find_all("div",{"style":"text-align: center; vertical-align: middle;"})
-# #<div id="table-forecast-type" class="col-lg-3 col-md-3 col-xs-12">
-# #print(containers[0])
-# # print(page_soup.td.text)
-# # table = page_soup.find("h3",{"class":"met-man-toggle"}).text
-
-# # print(x[0].text)
-# #print(table) 
-# # l=[]
-# # for items in table:
-# # #     for i in range(len(items.find_all("td"))-1):
-# #     d = {}  
-# #     d["Tarikh"]=items.find_all("td",{"style":"date-time"})[i].text
-# #     d["date"]=items.find_all("td",{"style":"day-detail"})[i].text
-# #     d["desc"]=items.find_all("td",{"style":"description"})[i].text 
-# #     d["temp"]=items.find_all("td",{"style":"temp"})[i].text 
-# #     d["precip"]=items.find_all("td",{"style":"precip"})[i].text
-# #     d["wind"]=items.find_all("td",{"style":"wind"})[i].text  
-# #     d["humidity"]=items.find_all("td",{"style":"humidity"})[i].text 
-# #     l.append(d)
-
-        
-# # page_soup.head
-# # page_soup.body.spa
-# # containers = page_soup.findAll("tr",{"style":"Pagi"})
-# # print (containers)
-
-# # contain = containers[0]
-# # container = containers[0]
-# # container.img["title"]
-# # container.strong.text.strip()
-# # print(f'The price : $ {priceC_0}')
-
-
-# # table=page_soup.find_all("table",{"id":"twc-forecast_table"})
-# # l=[]
-# # for items in table:
-# #  for i in items.find_all("tr"):
-
-# #      d = {}  
-# #      d["Tarikh"]=items.find_all("td",{"style":"text-align: center; vertical-align: middle;"})[i].text
-# #      d["Cuaca"]=items.find_all("td",{"style":"text-align: center; vertical-align: middle;"})[i].tex
-# #      d["Ramalan"]=items.find_all("td",{"style":"text-align: left; vertical-align: middle;"})[i].tex
-# #      l.append(d)
-
-# print(page_soup.find_all("td",{"style":"text-align: center; vertical-align: middle;"})[8].text)
